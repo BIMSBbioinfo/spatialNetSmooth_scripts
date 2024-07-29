3e423075cd36d50b27f683fd82c84f4573c39d19
Rahel <152613841+raheb61@users.noreply.github.com>
2024-07-29 15:35:26 +0200

    Update run_tests_ST.R

--- run_tests_ST.R
+++ run_tests_ST.R
@@ -5,12 +5,13 @@ library(dplyr)
 library(igraph)
 library(GSEABase)
 library(pROC)
+
+#load package (if not installed)
 setwd("~/spatialNetSmooth/")
-# F1 nochmal machen 
 devtools::load_all()
 # count matrix
 setwd("~/Documents")
-letter <- "H1"
+letter <- "H1" #select name of dataset
 datax <- read.table(paste("./data/ST-cnts/", letter,".tsv.gz", sep=""), header = TRUE, sep = "\t")
 entities <- datax$X
 datax <- datax[,-1]
@@ -63,13 +64,10 @@ params <- list(
 
 # make voltron object
 temp <- formVoltRon(data = datax, image = img, coords = coords, assay.type = "spot", params = params)
-#temp <- flipCoordinates(temp, assay="Custom_spot")
-#saveRDS(temp, "~/BA/object_ST.Rds")
-#temp <- readRDS("~/BA/object_ST.Rds")
+
 setwd(paste("~/BA/images ST/", letter, sep=""))
 seu <- gseaCalc(temp, assay="Custom_spot")
 coordinates <- coords
-#truth <- tumor
 gsea_raw <- as.matrix(Metadata(seu)$gsea_rat_norm)
 rownames(gsea_raw) <- rownames(Metadata(seu))
 #gsea_raw <- cbind(truth, gsea_raw)
@@ -78,7 +76,7 @@ rownames(gsea_raw) <- rownames(Metadata(seu))
                 
 F1_raw <- F1_quant(gsea_raw, truth, gsea_raw)
 pdf(paste("gsea_raw_",letter, ".pdf", sep=""))
-plot_quant(gsea_raw, coordinates, truth, 8, gsea_raw)
+plot_quant(gsea_raw, coordinates, truth)
 dev.off()
 alphas <- c(0.2, 0.4, 0.6, 0.8)
 
@@ -91,9 +89,9 @@ spatial <- as.data.frame(gsea_spatial)
 colnames(spatial) <- c("alpha=0.2", "alpha=0.4", "alpha=0.6", "alpha=0.8")
 write.csv(spatial, paste("spatial_scores_", letter, ".csv", sep=""), row.names = T)
 
-roc_spatial <- vector(mode = "list", length = 4)
+F1_spatial <- vector(mode = "list", length = 4)
 for (i in 1:4) {
-  roc_spatial[[i]] <- roc_quant(truth, spatial[,i])
+  F1_spatial[[i]] <- F1_quant(spatial[,i], truth, gsea_raw)
   
 }
 
@@ -106,9 +104,9 @@ nn <- as.data.frame(gsea_nn)
 colnames(nn) <- c("alpha=0.2", "alpha=0.4", "alpha=0.6", "alpha=0.8")
 write.csv(nn, paste("nn_scores_", letter, ".csv", sep=""), row.names = T)
 
-roc_nn <- vector(mode = "list", length = 4)
+F1_nn <- vector(mode = "list", length = 4)
 for (i in 1:4) {
-  roc_nn[[i]] <- roc_quant(truth, nn[,i])
+  F1_nn[[i]] <- F1_quant(nn[,i], truth, gsea_raw)
   
 }
 
@@ -128,9 +126,9 @@ nn_spatial<- as.data.frame(gsea_nn_spatial)
 colnames(nn_spatial) <- c("a1 = 0.2, a2=0.2","a1 = 0.2, a2=0.4", "a1 = 0.2, a2=0.6", "a1 = 0.2, a2=0.8", "a1 = 0.4, a2=0.2", "a1 = 0.4, a2=0.4", "a1 = 0.4, a2=0.6", "a1 = 0.4, a2=0.8", "a1 = 0.6, a2=0.2", "a1 = 0.6, a2=0.4", "a1 = 0.6, a2=0.6", "a1 = 0.6, a2=0.8", "a1 = 0.8, a2=0.2", "a1 = 0.8, a2=0.4", "a1 = 0.8, a2=0.6", "a1 = 0.8, a2=0.8")
 write.csv(nn_spatial, paste("nn_spatial_scores_", letter, ".csv", sep=""), row.names = T)
 
-roc_nn_spatial <- vector(mode = "list", length = 16)
+F1_nn_spatial <- vector(mode = "list", length = 16)
 for (i in 1:16) {
-  roc_nn_spatial[[i]] <- f1_beta(truth, nn_spatial[,i])
+  F1_nn_spatial[[i]] <- F1_quant(nn_spatial[,i], truth, gsea_raw)
   
 }
 
@@ -179,7 +177,7 @@ write.csv(alpha, paste("alpha_scores_", letter, ".csv", sep=""), row.names = T)
 
 F1_alpha <- vector(mode = "list", length = 16)
 for (i in 1:16) {
-  F1_alpha[[i]] <- f1_beta(alpha[,i], truth, gsea_raw)
+  F1_alpha[[i]] <- F1_quant(alpha[,i], truth, gsea_raw)
   
 }
 nn_df <- as.data.frame(cbind(F1_nn[[1]]$score, F1_nn[[2]]$score, F1_nn[[3]]$score, F1_nn[[4]]$score))
@@ -274,6 +272,7 @@ ggplot(df_long, aes(x = Position, y = Value, color = `Parameters`)) +
        y = "F1 Score")
 dev.off()
 
+#select best smoothing parameters for each method
 best_df <- as.data.frame(cbind(F1_nn[[4]]$score, F1_union[[4]]$score, F1_spatial[[4]]$score, F1_nn_spatial[[16]]$score, F1_alpha[[16]]$score))#removed F1_inter[[4]]$score
 colnames(best_df) <- c("snn", "union", "spatial", "snn_spatial", "alpha") #"intersection",
 best_df$Position <- 1:nrow(best_df)
@@ -288,29 +287,27 @@ ggplot(df_long, aes(x = Position, y = Value, color = `Method`)) +
        x = "Quantile as Threshold",
        y = "F1 Score")
 dev.off()
+truth <- as.vector(truth)
 pdf(paste("plot_snn_", letter, ".pdf", sep=""))
-plot_quant(gsea_nn[[4]], coordinates, truth, 8, gsea_raw)
+plot_quant(as.vector(gsea_nn[[4]]), coordinates, truth)
 dev.off()
 pdf(paste("plot_spatial_", letter, ".pdf", sep=""))
-plot_quant(gsea_spatial[[4]], coordinates, truth, 8, gsea_raw)
+plot_quant(as.vector(gsea_spatial[[4]]), coordinates, truth)
 dev.off()
 pdf(paste("plot_union_", letter, ".pdf", sep=""))
-plot_quant(gsea_union[[4]], coordinates, truth, 8, gsea_raw)
+plot_quant(as.vector(gsea_union[[4]]), coordinates, truth)
 dev.off()
 #pdf("plot_inter.pdf")
 #plot_quant(gsea_inter[[4]], coordinates, truth, 3, gsea_raw)
 #dev.off()
 
 pdf(paste("plot_alpha_", letter, ".pdf", sep=""))
-plot_quant(alpha[,16], coordinates, truth, 8, gsea_raw)
+plot_quant(as.vector(alpha[,16]), coordinates, truth)
 dev.off()
 pdf(paste("plot_snn_spatial", letter, ".pdf", sep=""))
-plot_quant(nn_spatial[,16], coordinates, truth, 7, gsea_raw)
+plot_quant(as.vector(nn_spatial[,16]), coordinates, truth)
 dev.off()
 
-pdf("plot_nn_spatial_2.pdf")
-plot_quant(nn_spatial[,12], coordinates, truth, 5, gsea_raw)
-dev.off()
 
 #compare_df <- as.data.frame(cbind(F1_nn_spatial_2[[12]]$score, F1_nn_spatial[[16]]$score))
 #colnames(compare_df) <- c("spatial first", "nn_first")
